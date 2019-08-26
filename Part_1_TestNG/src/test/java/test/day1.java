6163f2fbf274f5d3e2ae6be012a3a41afa6231a8
d0riangrey <utyfdfrthby08@gmail.com>
2019-08-26 23:14:23 +0300

    2

--- Part_1_TestNG/src/test/java/test/day1.java
+++ Part_1_TestNG/src/test/java/test/day1.java
@@ -8,30 +8,29 @@ import org.testng.annotations.Test;
 public class day1 {
 
 
-		// TODO Auto-generated method stub
-	@AfterTest
-	public void lastexecution()
-	{
-		System.out.println("I will execute last");
-		
-	}
-	@Test
-	public void Demo()
-	{
-		System.out.println("hello");//automation
-	}
-	@AfterSuite
-	
-	public void afSyite()
-	{
-		System.out.println("I am the no 1 from last ");
-	}
-	@Test
-	public void SecondTest()
-	{
-		System.out.println("bye");
-	}
+    // TODO Auto-generated method stub
+    @AfterTest
+    public void lastexecution() {
+        System.out.println("I will execute last");
 
+    }
+
+    @Test
+    public void Demo() {
+        System.out.println("hello");//automation
+        Assert.assertTrue(false);
+    }
+
+    @AfterSuite
+
+    public void afSyite() {
+        System.out.println("I am the no 1 from last ");
+    }
+
+    @Test
+    public void SecondTest() {
+        System.out.println("bye");
+    }
 
 
 }
