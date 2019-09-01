dd3116da8d961cc34404e06cb260aef15f3cd704
d0riangrey <utyfdfrthby08@gmail.com>
2019-09-01 22:45:09 +0300

    2

---
+++ Part_7_Logging_Framework_Log4j/src/test/java/alpha/Demo_1.java
@@ -0,0 +1,20 @@
+package alpha;
+
+
+import org.apache.logging.log4j.*;
+
+public class Demo_1 {
+
+    private static Logger log = LogManager.getLogger(Demo_1.class.getName());
+
+
+    public static void main(String[] args) {
+
+        log.debug("I have clicked on button");
+        log.info("Button is displayed");
+        log.error("Button is not displayed ");
+        log.fatal("button is missing");
+
+
+    }
+}
--- Part_7_Logging_Framework_Log4j/src/test/java/beta/Demo_2.java
+++ Part_7_Logging_Framework_Log4j/src/test/java/beta/Demo_2.java
@@ -1,11 +1,12 @@
-package alpha;
+package beta;
 
 
-import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Demo {
+public class Demo_2 {
 
-    private static Logger log = LogManager.getLogger(Demo.class.getName());
+    private static Logger log = LogManager.getLogger(Demo_2.class.getName());
 
 
     public static void main(String[] args) {
